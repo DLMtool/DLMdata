c0a97be3c86a60875168ba96c3208546ad603686
Adrian Hordyk <ar.hordyk@gmail.com>
2017-05-30 16:58:11 -0700

    add data objects

---
+++ R/Roxy_DataObjects.r
@@ -0,0 +1,149 @@
+# This file is automatically built by DataObjectsforDLMdata.r in DLMDev
+# Don't edit by hand!
+# 
+
+#'  testMSE OM
+#'
+#'  An object of class MSE
+#'
+"testMSE"
+
+
+#'  Red_Snapper_GOM OM
+#'
+#'  An object of class OM
+#'
+"Red_Snapper_GOM"
+
+
+#'  Rougheye_Rockfish_BC OM
+#'
+#'  An object of class OM
+#'
+"Rougheye_Rockfish_BC"
+
+
+#'  Yellowfin_Tuna_IO OM
+#'
+#'  An object of class OM
+#'
+"Yellowfin_Tuna_IO"
+
+
+#'  San_Francisco_Bay_herring_HerringSFB_OM OM
+#'
+#'  An object of class OM
+#'
+"San_Francisco_Bay_herring_HerringSFB_OM"
+
+
+#'  Darnley_Bay_Char_ArcticCharr_OM OM
+#'
+#'  An object of class OM
+#'
+"Darnley_Bay_Char_ArcticCharr_OM"
+
+
+#'  Canary_Rockfish_BC_CanaryRockfishBC_OM OM
+#'
+#'  An object of class OM
+#'
+"Canary_Rockfish_BC_CanaryRockfishBC_OM"
+
+
+#'  AJ_AlmacoJack_OM OM
+#'
+#'  An object of class OM
+#'
+"AJ_AlmacoJack_OM"
+
+
+#'  LS_LaneSnapper_OM OM
+#'
+#'  An object of class OM
+#'
+"LS_LaneSnapper_OM"
+
+
+#'  LAJ_LesserAmberjack_OM OM
+#'
+#'  An object of class OM
+#'
+"LAJ_LesserAmberjack_OM"
+
+
+#'  RD_RedDrum_OM OM
+#'
+#'  An object of class OM
+#'
+"RD_RedDrum_OM"
+
+
+#'  SG_SnowyGrouper_OM OM
+#'
+#'  An object of class OM
+#'
+"SG_SnowyGrouper_OM"
+
+
+#'  SH_SpeckledHind_OM OM
+#'
+#'  An object of class OM
+#'
+"SH_SpeckledHind_OM"
+
+
+#'  WM_Wenchman_OM OM
+#'
+#'  An object of class OM
+#'
+"WM_Wenchman_OM"
+
+
+#'  YM_YellowmouthGrouper_OM OM
+#'
+#'  An object of class OM
+#'
+"YM_YellowmouthGrouper_OM"
+
+
+#'  Swordfish_OM OM
+#'
+#'  An object of class OM
+#'  Author: Alex Hanke (DFO)  Assessment: http://iccat.org/Documents/SCRS/Manual/CH2/2_1_9_SWO_ENG.pdf
+#' 
+"Swordfish_OM"
+
+
+#'  Red_snapper_GOM_SS OM
+#'
+#'  An object of class OM (from SS using SS2DLM)
+#'  SEDAR 31. Author: SEDAR 31.
+#' 
+"Red_snapper_GOM_SS"
+
+
+#'  Yellowfin_tuna_IO_SS OM
+#'
+#'  An object of class OM (from SS using SS2DLM)
+#'  SEDAR 31. Author: SEDAR 31.
+#' 
+"Yellowfin_tuna_IO_SS"
+
+
+#'  Arrowtooth_Flounder_BC_SS OM
+#'
+#'  An object of class OM (from iSCAM using iSCAM2DLM)
+#'  DFO. Author: DFO.
+#' 
+"Arrowtooth_Flounder_BC_SS"
+
+
+#'  Rougheye_Rockfish_BC_SRA OM
+#'
+#'  An object of class OM (built using StochasticSRA)
+#'  DLMtool generated
+#' 
+"Rougheye_Rockfish_BC_SRA"
+
+
